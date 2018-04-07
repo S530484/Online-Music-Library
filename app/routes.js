5acdbdbce2a0840e8c5ff98b7be1b021dd0b8ce0
RaveendranathE <Ravee3011>
2018-04-07 15:05:44 -0500

    improved nav bar and connected to pages

--- app/routes.js
+++ app/routes.js
@@ -5,4 +5,25 @@ module.exports = function(app) {
     app.get('/', function(req, res) {
         res.render('Home.ejs');
     });
+
+    app.get('/aboutus', function(req, res) {
+        res.render('aboutus.ejs');
+    });
+
+    app.get('/contact', function(req, res) {
+        res.render('contact.ejs');
+    });
+
+    app.get('/music', function(req, res) {
+        res.render('music.ejs');
+    });
+
+    app.get('/signup', function(req, res) {
+        res.render('signup.ejs');
+    });
+
+    app.get('/login', function(req, res){
+        res.render('login.ejs')
+    });
+
 };
